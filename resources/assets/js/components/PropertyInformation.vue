8aaffd9e161191c9cdba607d55b0dec9aeab8a27
Neil Agor <ronielson.agor1@gmail.com>
2017-05-07 09:42:33 +0800

    changed map on modal. price and finance type are not repeating

--- resources/assets/js/components/PropertyInformation.vue
+++ resources/assets/js/components/PropertyInformation.vue
@@ -6,62 +6,73 @@
         <div class="container">
             <div class="row">
                 <p class="heading">Unit Details</p>
-                <div class="col-md-6">
-                    <img src="/img/user7-128x128.jpg" alt="" class="unit-full">
+                <div class="col-md-8">
+                    <img src="/img/house.jpg" alt="" class="unit-full">
                 </div>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <table class="table">
                         <tbody v-for="(item, index) in items" v-if="index == 0">
                             <tr>
-                                <td>Unit ID</td>
+                                <th>Unit ID</th>
                                 <td>{{ item.block_lot }}</td>
                             </tr>
                             <tr>
-                                <td>Lot Area</td>
+                                <th>Lot Area</th>
                                 <td>{{ item.lot_area }}</td>
                             </tr>
 
                             <tr>
-                                <td>Lot Type</td>
+                                <th>Lot Type</th>
                                 <td>{{ item.lot_type }}</td>
                             </tr>
                             <tr>
-                                <td>Floor Area</td>
+                                <th>Floor Area</th>
                                 <td>{{ item.floor_area }}</td>
                             </tr>
                             <tr>
-                                <td>House Model</td>
+                                <th>House Model</th>
                                 <td>{{ item.house_model }}</td>
                             </tr>
                             <tr>
-                                <td>Reservation Fee</td>
+                                <th>Reservation Fee</th>
                                 <td>{{ item.reservation_fee | currency }}</td>
                             </tr>
                         </tbody>
                     </table>
                 </div>
             </div>
+            <hr>
             <div class="row">
-                <p class="heading">Loan</p>
                 <div class="col-md-12">
                     <table class="table" v-for="(item, index) in items">
-                        <thead>
-                            <tr>
-                                <th>Finance Type</th>
-                                <th>Price</th>
-                                <th>Loan Total</th>
-                                <th>MRI</th>
-                                <th>Monthly Amortization</th>
-                                <th>Loan Percentage</th>
-                                <th>Description</th>
-                                <th>Monthly Percentage</th>
-                                <th>Years</th>
-                            </tr>
-                        </thead>
+                        <tr v-for="(loan, index1) in item.loans">
+                            <th v-if="index1 == 0" colspan="3">Finance Type</th>
+                            <th v-if="index1 == 0">Price</th>
+                        </tr>
+                        <tr v-for="(loan, index1) in item.loans">
+                            <td v-if="index1 == 0" colspan="3">{{ loan.finance_type }}</td>
+                            <td v-if="index1 == 0">{{ item.total_contract_price | currency }}</td>
+                        </tr>
+                        <tr>
+                            <td><br></td>
+                            <td><br></td>
+                            <td><br></td>
+                            <td><br></td>
+                            <td><br></td>
+                            <td><br></td>
+                            <td><br></td>
+                        </tr>
+                        <tr>
+                            <th>Loan Total</th>
+                            <th>MRI</th>
+                            <th>Monthly Amortization</th>
+                            <th>Loan Percentage</th>
+                            <th>Description</th>
+                            <th>Monthly Percentage</th>
+                            <th>Years</th>
+                        </tr>
                         <tbody>
                             <tr v-for="loan in item.loans">
-                                <td>{{ loan.finance_type }}</td>
-                                <td>{{ item.total_contract_price | currency }}</td>
                                 <td>{{ loan.total | currency }}</td>
                                 <td>{{ loan.mri | currency }}</td>
                                 <td>{{ loan.monthly_amortization | currency }}</td>
@@ -71,6 +82,9 @@
                                 <td>{{ loan.years }}</td>
                             </tr>
                         </tbody>
+                        <tr>
+                            <td colspan="7"><hr></td>
+                        </tr>
                     </table>
                 </div>
             </div>
