989979252c15c944b44a09cb8ad66caa24ecdc2c
Neil Agor <ronielson.agor1@gmail.com>
2017-05-08 11:06:31 +0800

    dynamic format. need to add columns for another loan record

--- app/Helpers/Handlers/RecordHandler.php
+++ app/Helpers/Handlers/RecordHandler.php
@@ -9,16 +9,10 @@ use App\Unit;
 class RecordHandler{
 	public static function createUnitEasy($line)
 	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+		$unit_active = self::getUnitActive($line[6], $line[19]);
 
 		if(! $unit_active){
-			$unit_easy = Unit::createUnitObject($line);
-
-			$dowpayment_easy = DownPayment::createEasy($unit_easy, $line);
-
-			$loans = Loan::createLoansEasy($unit_easy->id, $line);
-
-			return;
+			return $unit_easy = Unit::createUnitObject($line);
 		}
 
 		//this $unit_active must be archived
@@ -26,194 +20,190 @@ class RecordHandler{
 		
 		$to_be_archived->archive();
 		
-		$unit_easy = Unit::createUnitObject($line);
-		
-		$dowpayment_easy = Downpayment::createEasy($unit_easy, $line);
-
-		$loans = Loan::createLoansEasy($unit_easy->id, $line);
+		return $unit_easy = Unit::createUnitObject($line);
 	}
 
-	public static function createUnitLncBank2($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createUnitLncBank2($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_easy = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_easy = Unit::createUnitObject($line);
 
-			$dowpayment_easy = DownPayment::createLncBank2($unit_easy, $line);
+	// 		$dowpayment_easy = DownPayment::createLncBank2($unit_easy, $line);
 
-			$loans = Loan::createLoansEasy($unit_easy->id, $line);
+	// 		$loans = Loan::createLoansEasy($unit_easy->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_easy = Downpayment::createLncBank2($unit_lncBank2, $line);
+	// 	$dowpayment_easy = Downpayment::createLncBank2($unit_lncBank2, $line);
 
-		$loans = Loan::createLoanLncBank2($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoanLncBank2($unit_lncBank2->id, $line);
+	// }
 
-	public static function createUnitInHouse2($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createUnitInHouse2($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createInHouse2($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createInHouse2($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansInHouse2($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansInHouse2($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createInHouse2($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createInHouse2($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansInHouse2($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansInHouse2($unit_lncBank2->id, $line);
+	// }
 
-	public static function createUnitMoveIn12($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createUnitMoveIn12($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createMoveIn12($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createMoveIn12($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansMoveIn12($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansMoveIn12($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createMoveIn12($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createMoveIn12($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansMoveIn12($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansMoveIn12($unit_lncBank2->id, $line);
+	// }
 
-	public static function createMoveInAfter6($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createMoveInAfter6($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createMoveInAfter6($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createMoveInAfter6($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansMoveInAfter6($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansMoveInAfter6($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createMoveInAfter6($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createMoveInAfter6($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansMoveInAfter6($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansMoveInAfter6($unit_lncBank2->id, $line);
+	// }
 
-	public static function createZone2EquityTerm($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createZone2EquityTerm($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createZone2EquityTerm($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createZone2EquityTerm($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansZone2EquityTerm($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansZone2EquityTerm($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createZone2EquityTerm($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createZone2EquityTerm($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansZone2EquityTerm($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansZone2EquityTerm($unit_lncBank2->id, $line);
+	// }
 
-	public static function createZone2Bank2($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createZone2Bank2($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createZone2Bank2($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createZone2Bank2($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansZone2Bank2($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansZone2Bank2($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createZone2Bank2($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createZone2Bank2($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansZone2Bank2($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansZone2Bank2($unit_lncBank2->id, $line);
+	// }
 
-	public static function createZone2Inhouse2($line)
-	{
-		$unit_active = self::getUnitActive($line[6], $line[22]);
+	// public static function createZone2Inhouse2($line)
+	// {
+	// 	$unit_active = self::getUnitActive($line[6], $line[22]);
 
-		if(! $unit_active){
-			$unit_inHouse2 = Unit::createUnitObject($line);
+	// 	if(! $unit_active){
+	// 		$unit_inHouse2 = Unit::createUnitObject($line);
 
-			$dowpayment_inHouse2 = DownPayment::createZone2Inhouse2($unit_inHouse2, $line);
+	// 		$dowpayment_inHouse2 = DownPayment::createZone2Inhouse2($unit_inHouse2, $line);
 
-			$loans = Loan::createLoansZone2Inhouse2($unit_inHouse2->id, $line);
+	// 		$loans = Loan::createLoansZone2Inhouse2($unit_inHouse2->id, $line);
 
-			return;
-		}
+	// 		return;
+	// 	}
 
-		//this $unit_active must be archived
-		$to_be_archived = Unit::find($unit_active->unit_id);
+	// 	//this $unit_active must be archived
+	// 	$to_be_archived = Unit::find($unit_active->unit_id);
 		
-		$to_be_archived->archive();
+	// 	$to_be_archived->archive();
 		
-		$unit_lncBank2 = Unit::createUnitObject($line);
+	// 	$unit_lncBank2 = Unit::createUnitObject($line);
 		
-		$dowpayment_inHouse2 = Downpayment::createZone2Inhouse2($unit_lncBank2, $line);
+	// 	$dowpayment_inHouse2 = Downpayment::createZone2Inhouse2($unit_lncBank2, $line);
 
-		$loans = Loan::createLoansZone2Inhouse2($unit_lncBank2->id, $line);
-	}
+	// 	$loans = Loan::createLoansZone2Inhouse2($unit_lncBank2->id, $line);
+	// }
 
 	public static function getUnitActive($block_lot, $finance_type)
 	{
